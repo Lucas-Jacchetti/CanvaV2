ab54fdfacba16e07aa5240f746587c334bf6f653
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-06-06 16:08:37 -0300

    handleConnection

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -1,9 +1,22 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Server, Socket } from "socket.io";
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from "@nestjs/websockets";
+import { Logger } from "@nestjs/common";
 
-@WebSocketGateway()
-export class GameGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+@WebSocketGateway({ cors: true }) //escuta e responde a eventos de WebSocket
+export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect{
+  constructor(private gameService: GameService) {}
+
+  handleConnection(client: Socket) {
+    this.gameService.addPlayer(client.id); // adiciona jogador ao estado
+    client.emit('init', this.gameService.getGameState()); // envia estado atual para ele
+    client.broadcast.emit('newPlayer', { id: client.id }); // avisa aos outros que um novo jogador entrou
   }
+
 }
