47cbeb9245f84fc111b4374b8c7a4a7d3d106e77
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-06-09 15:22:48 -0300

    classe de testes

--- backend/src/game/game.gateway.spec.ts
+++ backend/src/game/game.gateway.spec.ts
@@ -1,18 +1,55 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { GameGateway } from './game.gateway';
+//*CLASSE DE TESTES
 
-describe('GameGateway', () => {
+import { Test } from "@nestjs/testing";
+import { GameGateway } from "./game.gateway";
+import { INestApplication } from "@nestjs/common";
+import { Socket, io } from "socket.io-client";
+
+async function createNestApp(...gateways: any): Promise<INestApplication> {
+  const testingModule = await Test.createTestingModule({ //recebe a gateway/provider e retorna um app nest 
+    providers: gateways,
+  }).compile();
+  return testingModule.createNestApplication();
+}
+
+describe("GameGateway", () => {
   let gateway: GameGateway;
+  let app: INestApplication;
+  let ioClient: Socket;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [GameGateway],
-    }).compile();
+  beforeAll(async () => {
+    app = await createNestApp(GameGateway); //antes de tudo, recebe a gateway/provider e retorna um app nest 
+    gateway = app.get<GameGateway>(GameGateway); //pega o gamegateway 
+    ioClient = io("http://localhost:3000", { //o local de conexão vair ser localhost3000, mas nao conecta direto pq autoconnect é false
+      autoConnect: false, //impede de conectar automaticamente
+      transports: ["websocket", "polling"],
+    });
 
-    gateway = module.get<GameGateway>(GameGateway);
+    app.listen(3000);
   });
 
-  it('should be defined', () => {
+  
+
+  it("should be defined", () => { //primeiro teste, checa se a instancia gateway (= gamegateway) existe
     expect(gateway).toBeDefined();
+  });
+
+  it('should emit "pong" on "ping"', async () => { //testa se o servidor responde a um evento 'ping' com 'pong'
+    ioClient.connect(); //conecta no webSocket
+    ioClient.emit("ping", "pingpong"); //emite o ping
+    await new Promise<void>((resolve) => { //espera a resposta 
+      ioClient.on("connect", () => {
+        console.log("connected");
+      });
+      ioClient.on("pong", (data) => {
+        expect(data).toBe("pingpong"); //checa se o pong recebido é igual ao ping
+        resolve();
+      });
+    });
+    ioClient.disconnect(); //desconecta o cliente
+  });
+
+  afterAll(async () => {
+    await app.close(); //fecha a aplicação
   });
 });
--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -14,32 +14,42 @@ import { Logger } from "@nestjs/common";
 export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect{ //interfaces para lidar com entrada e saida de jogadores
   constructor(private gameService: GameService) {}
 
-  private readonly logger = new Logger(GameGateway.name); // logger para registrar eventos
-  afterInit() {
-    this.logger.log("Initialized"); //indica que o gateway foi inicializado
-  }
+    private readonly logger = new Logger(GameGateway.name); // logger para registrar eventos
+    afterInit() {
+      this.logger.log("Initialized"); //indica que o gateway foi inicializado
+    }
 
-  handleConnection(client: Socket) { //gerencia a conexãod de um novo jogador
-    this.logger.log(`Client connected: ${client.id}`); //log temporario para registrar a conexão
-    
-    this.gameService.addPlayer(client.id); // adiciona jogador ao estado
-    client.emit('init', this.gameService.getGameState()); // envia estado atual para ele
-    client.broadcast.emit('newPlayer', { id: client.id }); // avisa aos outros que um novo jogador entrou
-  }
+    handleConnection(client: Socket) { //gerencia a conexãod de um novo jogador
+      this.logger.log(`Client connected: ${client.id}`); //log temporario para registrar a conexão
+      
+      this.gameService.addPlayer(client.id); // adiciona jogador ao estado
+      client.emit('init', this.gameService.getGameState()); // envia estado atual para ele
+      client.broadcast.emit('newPlayer', { id: client.id }); // avisa aos outros que um novo jogador entrou
+    }
 
-  handleDisconnect(client: Socket) { //gerencia a saidaa de um novo jogador
-    this.logger.log(`Client disconnected: ${client.id}`); //log temporario para registrar a desconexão
+    handleDisconnect(client: Socket) { //gerencia a saidaa de um novo jogador
+      this.logger.log(`Client disconnected: ${client.id}`); //log temporario para registrar a desconexão
 
-    this.gameService.removePlayer(client.id); // remove jogador
-    client.broadcast.emit('playerLeft', client.id); 
-  }
+      this.gameService.removePlayer(client.id); // remove jogador
+      client.broadcast.emit('playerLeft', client.id); 
+    }
 
-    @SubscribeMessage('jump')
+    @SubscribeMessage('jump') //nao seu fazer o jump ainda 
     handleJump(@ConnectedSocket() client: Socket) { //*arrumar parametros eu acho
       this.gameService.jumpPlayer(client.id);
 
       const state = this.gameService.getGameState();
       client.broadcast.emit('state', state);
+    }
+    
+    @SubscribeMessage("ping")
+    handleMessage(client: any, data: any) {
+      this.logger.log(`Message received from client id: ${client.id}`);
+      this.logger.debug(`Payload: ${data}`);
+      return {
+        event: "pong",
+        data,
+      };
     }
 
 }
