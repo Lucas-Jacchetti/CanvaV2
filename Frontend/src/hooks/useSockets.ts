acb265a77b638bce6162d8a79cc8a1cf05aae269
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-07-01 23:35:55 -0300

    eventos recebidos do servidor no useSockets

---
+++ Frontend/src/hooks/useSockets.ts
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from "react";
+import { io, Socket } from "socket.io-client";
+import type { GameState, RankingEntry } from "../types";
+//ranking
+//playerId
+//finishTime
+export function useSocket(name: string){
+    const socketRef = useRef<Socket | null>(null);
+    const [gameState, setGameState] = useState< GameState | null>(null); 
+    const [ranking, setRanking] = useState< RankingEntry[] >([]);
+    const [playerId, setPlayerId] = useState< number | null >(null);
+    const [finishTime, setFinishTime] = useState< string | null >(null);
+
+    useEffect(() => {
+        const socket = io("http://localhost:3000");
+        socketRef.current = socket;
+
+        socket.emit("join", { name} );
+
+        socket.on("init", (state: GameState) => {
+            setGameState(state);
+            setPlayerId(Number(socket.id));
+        })
+
+        socket.on("state", (state: GameState) => {
+            setGameState(state);
+        })
+
+        socket.on("rankingUpdate", (rankingData: RankingEntry[]) => {
+            setRanking(rankingData);
+        })
+
+        socket.on("finishTime", ({time}: { time: number }) => {
+            setFinishTime(time.toString());
+        })
+
+        return () => {
+            socket.disconnect();
+        };
+    }, [name]);
+
+    
+}
