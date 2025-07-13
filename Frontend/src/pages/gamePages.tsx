a3ed748c62733ca87977fccaadb4cdcdd4e784be
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-07-13 11:31:50 -0300

    bug do reset resolvido

--- Frontend/src/pages/gamePages.tsx
+++ Frontend/src/pages/gamePages.tsx
@@ -60,7 +60,10 @@ function GamePage(){
                 <div className="flex gap-4 mt-2">
                     <button
                     className="bg-red-600 hover:bg-red-700 px-4 py-2 rounded cursor-pointer"
-                    onClick={resetGame}
+                    onClick={(e) => {
+                        e.currentTarget.blur();
+                        resetGame();
+                    }}
                     >
                     Resetar Partida
                     </button>
