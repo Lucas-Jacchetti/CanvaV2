3e22420f5776d7239552f095d2d34d56e7b45b93
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-07-13 17:55:58 -0300

    namePage ajustes

--- Frontend/src/pages/namePage.tsx
+++ Frontend/src/pages/namePage.tsx
@@ -14,10 +14,10 @@ function NamePage(){
     return(
         <>
         <div className="flex justify-center items-center bg-gray-900">
-            <div className="flex flex-col justify-center items-center bg-white gap-8 border-2 rounded-2xl w-100 mt-35 mb-10">
+            <div className="flex flex-col justify-center items-center bg-gray-300 gap-8 border-0 rounded-2xl w-100 mt-35 mb-10">
                 <div className="text-3xl font-bold mt-8">Enter your nickname:</div>
                 <input type="text" value={nickname} onChange={(e) => setNickname(e.target.value)} className="border-2 rounded bg-gray-100 w-60 h-auto"/>
-                <button onClick={handleStart} className="border-0 w-20 h-10 cursor-pointer bg-blue-400 rounded mb-15 font-bold">Start</button>
+                <button onClick={handleStart} className="border-0 w-20 h-10 cursor-pointer bg-green-500 rounded mb-15 font-bold">Start</button>
             </div>
         </div>
         </>
