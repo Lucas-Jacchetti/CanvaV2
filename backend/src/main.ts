6639d857c8e875e140c0791f435c73464538a31a
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-07-24 20:36:52 -0300

    vercel v2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // frontend local (Vite)
+      'https://canva-v2-iota.vercel.app' // substitua pelo domínio real do seu frontend
+    ],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
