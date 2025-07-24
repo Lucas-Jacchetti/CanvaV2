5ca2f05dc312f62109754c2d745283754832926e
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-07-24 14:17:16 -0300

    migração do banco para postgresql

--- backend/prisma/migrations/20250713143340_dev/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "RankingEntry" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "playerId" TEXT NOT NULL,
-    "playerName" TEXT NOT NULL,
-    "time" INTEGER NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
