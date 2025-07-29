ce45ddb7a9b4f41a08be01a06a21c2bb3db0f699
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-07-29 00:55:22 -0300

    front 3

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,7 +1,10 @@
 // app.controller.ts
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  
+    @Get()
+    healthCheck(){
+        console.log("Backend running")
+    }
 }
